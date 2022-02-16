72d04ac47533dfb9c9bf4a20729e131a4d4f9b91
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-02-16 23:58:43 +0530

    Time: 8 ms (68.98%), Space: 6.9 MB (75.08%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int m=s.length();
+        int m= s.length();
         int n=t.length();
         if(m!=n) return false;
         int scount[256]={0};
         int tcount[256]={0};
+        
         for(int i=0;i<n;i++)
         {
             if(!scount[s[i]] and !tcount[t[i]])
@@ -13,9 +14,9 @@ public:
                 scount[s[i]]=t[i];
                 tcount[t[i]]=s[i];
             }
-            else if(scount[s[i]] != t[i])
-                return false; 
+            else if(scount[s[i]]!= t[i])
+                return false;
         }
-        return true;
+        return true;        
     }
 };
