ec823366f957993ef4e899c2aebe4b3720603038
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-07-28 16:19:02 +0530

    Time: 24 ms (19.33%), Space: 7.4 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n1=s.size();
+        int n2=t.size();
+        if(n1!=n2)
+            return 0;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<n1;i++)
+        {
+            if(s[i]!=t[i])
+                return 0;
+        }
+        return 1;
+    }
+};
