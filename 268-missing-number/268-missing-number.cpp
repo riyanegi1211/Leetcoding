7ec00645d0e79b8ce0b21544f30447a75cc98c76
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-07-29 12:44:36 +0530

    Time: 31 ms (45.83%), Space: 18 MB (18.76%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++)
         {
-            if(nums[i]!=i )
+            if(nums[i]!=i)
                 return i;
         }
         return n;
