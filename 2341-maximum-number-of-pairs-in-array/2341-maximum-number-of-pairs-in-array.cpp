df945eb8984a1cac56c0927c1e79cd7882213da9
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-08-01 18:39:09 +0530

    Time: 10 ms (14.37%), Space: 9 MB (95.30%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& nums) {
+        vector<int>ans;
         sort(nums.begin(),nums.end());
-        int pair = 0;
-        
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]){
+        int n=nums.size();
+        int pair=0;
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
                 pair++;
                 i++;
             }
         }
-        
-        int left = nums.size()-(2*pair);
-        return {pair,left};
+        int rem=n-(2*pair);
+        ans.push_back(pair);
+        ans.push_back(rem);
+        return ans;
     }
 };
