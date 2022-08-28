c072f1eca4067c82b29af667f93028d06375b2a9
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-08-28 11:55:54 +0530

    Time: 349 ms (42.40%), Space: 10.3 MB (70.50%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int countPrimes(int n) {        
         int count=0;
-        if(n==0 || n==1) return 0;
+        if(n<=1) 
+            return 0;
         vector<bool> vec(n+1, 1);              
         for(int i=2; i<n; i++)
         {
