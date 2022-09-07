b4bbcfb96935a2eac581addc73096bd407a2b215
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-09-07 12:55:23 +0530

    Time: 3 ms (60.41%), Space: 8.5 MB (41.82%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -10,19 +10,18 @@
  * };
  */
 class Solution {
-public:
     void preorder(vector<int>&ans, TreeNode* root)
     {
-        if(!root)
+        if(root==NULL)
             return;
-        
         ans.push_back(root->val);
         preorder(ans,root->left);
         preorder(ans,root->right);
     }
+public:
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>v;
-        preorder(v, root);
-        return v;        
+        vector<int>ans;
+        preorder(ans,root);
+        return ans;
     }
 };
