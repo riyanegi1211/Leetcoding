bc71b30f06279e18a6a9ed8201b83a9a273ee755
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-09-11 20:01:12 +0530

    Time: 7 ms (56.14%), Space: 8.4 MB (27.35%) - LeetHub

---
+++ 682-baseball-game/682-baseball-game.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int calPoints(vector<string>& ops) {
+        stack<int>v;
+        for(int i=0;i<ops.size();i++){
+            if(ops[i]=="C"){
+                v.pop();
+            }
+            
+            else if(ops[i]=="D"){
+                int ans=v.top();
+                ans*=2;
+                v.push(ans);
+            }
+            
+            else if(ops[i]=="+"){
+                int first=v.top();
+                v.pop();
+                int second=v.top();
+                v.pop();
+                 v.push(second);
+                 v.push(first);
+                 v.push(first+second);
+            }
+            
+            else{
+               int number=stoi(ops[i]);
+                v.push(number);
+            }
+        }
+        int sum=0;
+        while(!v.empty()){
+            int top=v.top();
+            sum=sum+top;
+             v.pop();
+        }
+        
+        return sum;
+    }
+};
