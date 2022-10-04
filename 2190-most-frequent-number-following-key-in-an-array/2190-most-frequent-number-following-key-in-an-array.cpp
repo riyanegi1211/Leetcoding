0768c2ffd2e688f5e374ab2ac6b494142a8fe641
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-10-05 00:00:04 +0530

    Time: 18 ms (60.84%), Space: 12.7 MB (63.22%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -3,16 +3,20 @@ public:
     int mostFrequent(vector<int>& nums, int key) {
         unordered_map<int,int>mp;
         int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            if(nums[i]==key){
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i]==key)
+            {
                 int temp=nums[i+1];
                 mp[temp]++;
             }
         }
         int maxi=0;
         int ans=0;
-        for(auto it:mp){
-            if(it.second>maxi){
+        for(auto it:mp)
+        {
+            if(it.second>maxi)
+            {
              maxi=it.second;
              ans=it.first;
             }
