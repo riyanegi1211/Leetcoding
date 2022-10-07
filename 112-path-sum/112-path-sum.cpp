597de2ab73e9673914f9b5048ebd1f28f73f0ce5
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2022-10-07 19:45:00 +0530

    Time: 23 ms (43.59%), Space: 21.4 MB (38.36%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -14,8 +14,8 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root)
             return false;
-        if(!root->left && !root->right)
-            return root->val==targetSum;
+        if(!root->left && !root->right && root->val==targetSum)
+            return true;
         return
             hasPathSum(root->left, targetSum-root->val) || 
             hasPathSum(root->right,targetSum-root->val);
