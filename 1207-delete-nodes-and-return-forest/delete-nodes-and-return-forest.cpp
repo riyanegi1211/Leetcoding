f8d6ea0c68678e26a59389238e3e383ccd86e177
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2024-07-17 22:02:14 +0530

    Time: 13 ms (74.05%) | Memory: 27.5 MB (18.42%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        unordered_set<int> toDeleteSet(to_delete.begin(),to_delete.end());
+        vector<TreeNode*> forest;
+
+        root = processNode(root,toDeleteSet,forest);
+        if(root){
+            forest.push_back(root);
+        }
+        return forest;
+    }
+private:
+    TreeNode* processNode(TreeNode* node,unordered_set<int>& toDeleteSet,vector<TreeNode*>&forest){
+        if(!node){
+            return nullptr;
+        }
+
+        node->left = processNode(node->left,toDeleteSet,forest);
+        node->right = processNode(node->right,toDeleteSet,forest);
+
+        if(toDeleteSet.find(node->val)!= toDeleteSet.end()){
+            if(node->left){
+                forest.push_back(node->left);
+            }
+            if(node->right){
+                forest.push_back(node->right);
+            }
+            delete node;
+            return nullptr;
+        }
+        return node;
+    }
+};
