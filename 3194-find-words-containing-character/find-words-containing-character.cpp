5e405350d6777dc0007cc477ab669e409a51bc23
Riya Negi <58062535+riyanegi1211@users.noreply.github.com>
2024-07-20 17:43:52 +0530

    Time: 17 ms (62.51%) | Memory: 33.3 MB (30.65%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -4,11 +4,8 @@ public:
         vector<int>count;
         int n = words.size();
         for(int i =0;i<n;i++){
-            for(auto j:words[i]){
-                if(j==x){
-                    count.push_back(i);
-                    break;
-                }
+            if(words[i].contains(x)){
+                count.push_back(i);
             }
         }
         return count;
